24d351f10d27bed12b6ed9077bb2c4e769adeff9
ADMIN <ADMIN@DESKTOP-EITBVLV>
2022-12-01 18:32:38 +0530

    Activity3 1-22

---
+++ Activity3/src/com/question2/FindSequences.java
@@ -0,0 +1,37 @@
+ //author:rohit
+// code to find lower case with underscore
+package com.question2;
+
+public class FindSequences {
+
+	public static void main(String[] args) {
+		 String s="java_activity";
+		
+		 boolean b=false;
+		 for(int i=0;i<s.length();i++) {
+			 
+			 if(s.charAt(i)=='_')
+			 {
+				 char c=s.charAt(i+1);
+				 if (Character.isLowerCase(c))
+				 {
+					 b=true;
+					 break;
+				 }
+			 }
+		 }
+		if(b)
+			System.out.println("matched");
+		else
+			System.out.println("not matched");
+	}
+
+		
+		
+		
+		
+		
+		
+	}
+
+
